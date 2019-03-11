22d2a283de4903e7633f9332e7bae4f8b0484bb3
dbstjrgh0215 <dbstjrgh0215@naver.com>
2019-03-11 13:10:16 +0900

    first

---
+++ src/main/java/com/scit/flee2/HomeController.java
@@ -0,0 +1,20 @@
+package com.scit.flee2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		return "home";
+	}
+	
+	@RequestMapping(value="/goLogin", method=RequestMethod.GET)
+	public String goLogin() {
+		return "login";
+	}
+	
+}
